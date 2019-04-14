aa203c9af98b107a50082a215fbf04130f05a63b
Kyung Jin Cho <kj_cho@idealidea.co.kr>
2019-04-14 14:37:38 +0900

    연습문제/응용문제까지 작성

--- app/src/main/java/com/tjeit/a20190414_goodoc_main_copy/MainActivity.java
+++ app/src/main/java/com/tjeit/a20190414_goodoc_main_copy/MainActivity.java
@@ -2,12 +2,35 @@ package com.tjeit.a20190414_goodoc_main_copy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText searchKeywordEdt;
+    Button searchBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        searchKeywordEdt = findViewById(R.id.searchKeywordEdt);
+        searchBtn = findViewById(R.id.searchBtn);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String keyword = searchKeywordEdt.getText().toString();
+
+                String toastMessage = String.format("[%s]을 검색합니다.", keyword);
+
+                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
