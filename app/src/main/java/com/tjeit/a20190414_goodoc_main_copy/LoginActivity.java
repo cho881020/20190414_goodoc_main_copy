fef3963d0b6ae183e6659a384bfb14716213b91b
Kyung Jin Cho <kj_cho@idealidea.co.kr>
2019-04-14 15:39:46 +0900

    메인화면 이동시 로그인 화면 종료

--- app/src/main/java/com/tjeit/a20190414_goodoc_main_copy/LoginActivity.java
+++ app/src/main/java/com/tjeit/a20190414_goodoc_main_copy/LoginActivity.java
@@ -46,8 +46,7 @@ public class LoginActivity extends AppCompatActivity {
 
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
-
-
+                    finish();
 
                 }
                 else {
